b4b477a08b6e576ec6dbab237642ec4a051bf42a
Ckwei <kkww0259@gmail.com>
2020-04-01 17:50:55 +0800

    add main.cpp

--- 1_uLCD/main.cpp
+++ 1_uLCD/main.cpp
@@ -2,6 +2,7 @@
 #include "uLCD_4DGL.h"
 
 uLCD_4DGL uLCD(D1, D0, D2);
+PwmOut PWM1(D6);
 
 int main()
 {
@@ -9,5 +10,21 @@ int main()
       uLCD.text_width(2); 
       uLCD.text_height(2);
       uLCD.printf("\n107061213\n"); //Default Green on black text
-      
+
+      PWM1.period(0.001);
+      PWM1 = 0;
+      /*while(1){
+          if(PWM1 == 0){
+              for(float i=0;i<=1;i+=0.1){
+                  PWM1+=0.1;
+                  wait(0.1);
+              }
+          }
+          if(PWM1 == 1){
+              for(i=1;i>=0;i-=0.1){
+                  PWM1-=0.1;
+                  wait(0.1);
+              }
+          }
+      }*/
 }

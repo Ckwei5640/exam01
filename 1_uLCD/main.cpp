7afc78ff2c9b909daefaeb6b7237a18510f24e79
Ckwei <kkww0259@gmail.com>
2020-04-01 17:07:18 +0800

    add main.cpp

--- 1_uLCD/main.cpp
+++ 1_uLCD/main.cpp
@@ -5,7 +5,7 @@ uLCD_4DGL uLCD(D1, D0, D2);
 
 int main()
 {
-      uLCD.rectangle(32,32,96,96,WHITE);
+      uLCD.rectangle(32,32,96,96,WHITE); //start x,y; end x,y
       uLCD.text_width(2); 
       uLCD.text_height(2);
       uLCD.printf("\n107061213\n"); //Default Green on black text
